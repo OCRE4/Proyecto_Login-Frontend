da2689acdcef65ae258c95611876b7505996054f
Lauta <lautarobarrera150@gmail.com>
2023-02-05 13:12:50 -0300

    primera subida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,51 @@
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule,  HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { AuthInterceptorService } from './shared/auth-interceptor.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '/login', component: SigninComponent },
+  { path: '/register', component: SignupComponent },
+  { path: '/profile', component: UserProfileComponent },
+
+];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
+
+  imports: [RouterModule.forRoot(routes),
+
+    BrowserAnimationsModule,
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule
+
   ],
-  providers: [],
+  exports: [RouterModule],
+
+  declarations: [
+    AppComponent,
+    SigninComponent,
+    SignupComponent,
+    UserProfileComponent
+  ],
+
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true
+    }
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class AppRoutingModule {}
